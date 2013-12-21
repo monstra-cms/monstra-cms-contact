14c7490ad2864cb5c39f8130013626aa33beedc9
Awilum <awilum@msn.com>
2013-12-22 00:56:59 +0200

    Contact 1.1.1

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-monstra-cms-contact
+Contact
 ===================
+
+Simple contact plugin for Monstra CMS
+
+Usage:
+
+Shordcode for content
+```
+{contact recipient="admin@site.org"}
+```
+
+Code for templates
+```
+<?php Contact::display('admin@site.org'); ?>
+```
---
+++ views/frontend/form.view.php
@@ -0,0 +1,28 @@
+<?php if (Notification::get('success')) Alert::success(Notification::get('success')); ?>
+<?php if (Notification::get('error')) Alert::error(Notification::get('error')); ?>
+<br />
+<form method="post">
+    <?php echo (Form::hidden('csrf', Security::token())); ?>
+    <label><?php echo __('Name', 'contact'); ?></label>
+    <input  type="text" name="contact_name" class="input-xlarge" value="<?php echo $name; ?>" /><br />
+    <label><?php echo __('Email', 'contact'); ?></label>
+    <input  type="text" name="contact_email" class="input-xlarge" value="<?php echo $email; ?>" /><br />
+    <label><?php echo __('Message', 'contact'); ?></label>
+    <textarea class="input-xxlarge" rows="10" name="contact_body"><?php echo $body; ?></textarea><br /><br />
+
+    <?php if (Option::get('captcha_installed') == 'true') { ?>
+    <label><?php echo __('Captcha', 'users'); ?><label>
+    <input type="text" name="answer"><?php if (isset($errors['captcha_wrong'])) echo Html::nbsp(3).'<span class="error">'.$errors['captcha_wrong'].'</span>'; ?>
+    <?php CryptCaptcha::draw(); ?>
+    <?php } ?>
+
+    <br />
+    <?php if (count($errors) > 0) { ?>
+    <ul>
+    <?php foreach ($errors as $error) { ?>
+        <li><?php echo $error; ?></li>
+    <?php } ?>
+    </ul>
+    <?php } ?>
+    <input type="submit" class="btn" value="<?php echo __('Send', 'contact'); ?>" name="contact_submit"/>
+</form>
